5e66f081910bd309e480f2f544ad9731d585fd82
Voidable <randomkingwholikespink@gmail.com>
2015-12-22 17:01:02 -0500

    commit 5:00PM 12/22/2015

--- Dafinchi/Form1.Designer.cs
+++ Dafinchi/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dafinchi
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblCredit = new System.Windows.Forms.Label();
+            this.lstConsole = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblCredit
+            // 
+            this.lblCredit.AutoSize = true;
+            this.lblCredit.Location = new System.Drawing.Point(12, 440);
+            this.lblCredit.Name = "lblCredit";
+            this.lblCredit.Size = new System.Drawing.Size(181, 13);
+            this.lblCredit.TabIndex = 0;
+            this.lblCredit.Text = " Created by Keegon Cabinaw  - 2015";
+            // 
+            // lstConsole
+            // 
+            this.lstConsole.FormattingEnabled = true;
+            this.lstConsole.Location = new System.Drawing.Point(12, 12);
+            this.lstConsole.Name = "lstConsole";
+            this.lstConsole.Size = new System.Drawing.Size(500, 199);
+            this.lstConsole.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.lstConsole);
+            this.Controls.Add(this.lblCredit);
+            this.Name = "FrmMain";
+            this.Text = "Dafinchi!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCredit;
+        private System.Windows.Forms.ListBox lstConsole;
     }
 }
 
--- Dafinchi/Form1.cs
+++ Dafinchi/Form1.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace Dafinchi
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        /// <summary>
+        /// Default Form Constructor
+        /// </summary>
+        public FrmMain()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Public method to write messages to the Console Listbox
+        /// </summary>
+        /// <param name="message">String to be writen to Listbox</param>
+        public void WriteLineToConsoleList(string message)
+        {
+            //  Write message to listbox, move the view to the bottom of the list, then unselect to clear the blue line.
+            lstConsole.Items.Add(message);
+            lstConsole.SelectedIndex = lstConsole.Items.Count - 1;
+            lstConsole.SelectedIndex = -1;
         }
     }
 }
